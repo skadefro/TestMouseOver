0d138649fcab203eec9c090ec7d8358bf69a9acf
Allan Zimmermann <allan@openrpa.dk>
2022-09-23 17:15:26 +0200

    Add project files.

---
+++ TestMouseOver/NativeMethods.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMouseOver
+{
+    public static class NativeMethods
+    {
+        public const Int32 HC_ACTION = 0;
+        public const Int32 WH_KEYBOARD_LL = 13;
+        public const Int32 WH_MOUSE_LL = 14;
+        public const Int32 WM_MOUSEMOVE = 0x0200;
+        public struct POINT
+        {
+            public int x;
+            public int y;
+        }
+        public delegate IntPtr LLProc(Int32 nCode, IntPtr wParam, IntPtr lParam);
+        [DllImport("user32.dll")]
+        public static extern IntPtr SetWindowsHookEx(Int32 idHook, LLProc lpfn, IntPtr hMod, UInt32 dwThreadId);
+        [DllImport("user32.dll")]
+        public static extern bool UnhookWindowsHookEx(IntPtr hhk);
+        [DllImport("user32.dll")]
+        public static extern IntPtr CallNextHookEx(IntPtr hhk, Int32 nCode, IntPtr wParam, IntPtr lParam);
+        [DllImport("user32.dll")]
+        public static extern Boolean GetPhysicalCursorPos(ref POINT pt);
+        [DllImport("user32.dll")]
+        public static extern bool SetCursorPos(int X, int Y);
+
+
+        [Serializable]
+        public struct MSG
+        {
+            public IntPtr hwnd;
+
+            public IntPtr lParam;
+
+            public int message;
+
+            public int pt_x;
+
+            public int pt_y;
+
+            public int time;
+
+            public IntPtr wParam;
+        }
+        [DllImport("user32.dll")]
+        public static extern bool GetMessage(out MSG lpMsg, IntPtr hWnd, uint wMsgFilterMin, uint wMsgFilterMax);
+
+        [DllImport("user32.dll")]
+        public static extern bool TranslateMessage([In] ref MSG lpMsg);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr DispatchMessage([In] ref MSG lpmsg);
+
+    }
+}
---
+++ TestMouseOver/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Drawing;
+using static TestMouseOver.NativeMethods;
+using System.Windows.Automation;
+using FlaUI.Core.Definitions;
+using System.Diagnostics;
+using System.Xml.Linq;
+
+namespace TestMouseOver
+{
+    internal class Program
+    {
+
+        private static NativeMethods.LLProc mouseProc;
+        private static IntPtr mouseHook;
+        public static bool doCrash = true;
+        private static IntPtr LowLevelMouseProc(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            try
+            {
+                if ((int)wParam == NativeMethods.WM_MOUSEMOVE)
+                {
+                    var pt = new NativeMethods.POINT();
+                    NativeMethods.GetPhysicalCursorPos(ref pt);
+                    if(doCrash)
+                    {
+
+                        using (var automation = new FlaUI.UIA3.UIA3Automation())
+                        {
+                            var ele = automation.FromPoint(new Point(pt.x, pt.y));
+                            Console.WriteLine(pt.x + "," + pt.y + " processid: " + ele.Properties.ProcessId.Value);
+                            var ProcessId2 = ele.Properties.ProcessId.ValueOrDefault;
+                            var Name2 = ele.Properties.Name.ValueOrDefault;
+                            var ClassName2 = ele.Properties.ClassName.ValueOrDefault;
+                            var Type2 = ele.Properties.ControlType.ValueOrDefault.ToString();
+                            var FrameworkId2 = ele.Properties.FrameworkId.ValueOrDefault;
+                        }
+                        //AutomationElement element = AutomationElement.FromPoint(new System.Windows.Point(pt.x, pt.y));
+                        //Console.WriteLine(pt.x + "," + pt.y + " processid: " + element.Current.ProcessId);
+                        //var ProcessId = element.Current.ProcessId;
+                        //var Name = element.Current.Name;
+                        //var ClassName = element.Current.ClassName;
+                        //var Type = element.Current.ControlType.ToString();
+                        //var FrameworkId = element.Current.FrameworkId.ToString();
+
+
+                    }
+                    else
+                    {
+                        // Console.WriteLine(pt.x + "," + pt.y);
+                        AutomationElement element = AutomationElement.FromPoint(new System.Windows.Point(pt.x, pt.y));
+                        Console.WriteLine(pt.x + "," + pt.y + " processid: " + element.Current.ProcessId 
+                            + " " + element.Current.ClassName.ToString() + " " + element.Current.ControlType.ToString());
+
+                        using (var automation = new FlaUI.UIA3.UIA3Automation())
+                        {
+                            var ele = automation.FromPoint(new Point(pt.x, pt.y));
+                            Console.WriteLine(pt.x + "," + pt.y + " processid: " + ele.Properties.ProcessId.ValueOrDefault
+                                + " " + ele.Properties.ClassName.ValueOrDefault?.ToString() + " " + ele.Properties.ControlType.ValueOrDefault.ToString());
+                        }
+                    }
+
+                }
+                return NativeMethods.CallNextHookEx(IntPtr.Zero, nCode, wParam, lParam);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return IntPtr.Zero;
+            }
+        }
+        static void Main(string[] args)
+        {
+            bool haderror = false;
+            try
+            {
+                mouseProc = LowLevelMouseProc;
+                mouseHook = NativeMethods.SetWindowsHookEx(NativeMethods.WH_MOUSE_LL, mouseProc, IntPtr.Zero, 0);
+                Console.WriteLine("Press ctrl+C to exit");
+                NativeMethods.MSG msg;
+                while ((!NativeMethods.GetMessage(out msg, IntPtr.Zero, 0, 0)))
+                {
+                    NativeMethods.TranslateMessage(ref msg);
+                    NativeMethods.DispatchMessage(ref msg);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                haderror = true;
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                NativeMethods.UnhookWindowsHookEx(mouseHook);
+            }
+            Console.WriteLine("Done");
+            if(haderror) Console.ReadKey();
+
+        }
+    }
+}
